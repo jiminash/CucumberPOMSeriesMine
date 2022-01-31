e2eb62b400bfa24e332f260eee3bc40d4607aed9
jiminash <35187476+jiminash@users.noreply.github.com>
2022-01-31 21:24:45 +0530

    Update DriverFactory.java

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
-
+// adde din driver
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
